7f495aec7ebeb68f7b53b7f250f89b1ee4fa1511
Iam-samyog <msamyog37@gmail.com>
2025-05-20 22:04:43 +0545

    added UnaryOpp.cpp and qn1.cpp

---
+++ C++ codes/Day 1/UnaryOpp.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=5;
+    cout << a << endl;
+
+    a++;
+    cout << a << endl;
+    
+    cout << a++ << endl;
+    cout << a << endl;
+    cout << ++a << endl;
+    cout << a << endl;
+
+    cout <<"------"<<endl;
+    cout << a-- << endl;
+    cout << a << endl;
+    cout << --a << endl;
+    cout << a << endl;
+
+
+
+    return 0;
+}
---
+++ C++ codes/Day 1/qn1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1,n2;
+
+    cout<<"Enter Number n1 and n2:";
+    cin >> n1 >>n2;
+
+    cout <<"Sum of " << n1 <<" and "<<n2 <<" is "<<(n1+n2);
+    return 0;
+}
+
