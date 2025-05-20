a02e67b8c9e60052ee4740fba0326c43a23ef752
Iam-samyog <msamyog37@gmail.com>
2025-05-20 21:43:54 +0545

    Added Operators.c and changed the file location

---
+++ C++ codes/Day 1/Input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    double price;
+    cout << "Enter your age:";
+    cin >> age;
+    
+    cout <<"Enter your Price:";
+    cin >> price;
+
+    cout << "Your age is: "<< age << endl;
+    cout <<"Price is :" << price << endl;
+    return 0;
+}
---
+++ C++ codes/Day 1/Operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout <<"Enter a and b:";
+    cin >> a >> b;
+   
+   //Arithmetic operator
+    cout << "Sum is :" << (a+b) <<endl;
+    cout << "Difference is :" << (a-b) <<endl;
+    cout << "Multiply is :" << (a*b) <<endl;
+    cout << "divide is :" << (a/b) <<endl;
+    cout << "Module is :" << (a%b) <<endl;
+
+    
+
+    return 0;
+}
