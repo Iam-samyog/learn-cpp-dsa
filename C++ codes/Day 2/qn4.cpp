ee298394bb5ba10afb3d9dddaba9922db1bc99a5
Iam-samyog <msamyog37@gmail.com>
2025-05-21 22:50:31 +0545

    Added qn4.cpp

---
+++ C++ codes/Day 2/qn4.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+//Check if a number is prime or not
+int main(){
+    int num;
+    bool isPrime=1;
+    cout << "Enter Number :";
+    cin >> num;
+
+   if(num <=1 ){
+    cout << "Number is less than 1";
+
+   }
+   else{
+    for(int i=2;i<num;i++){
+        if(num%i==0){
+            isPrime=0;
+
+        }
+    }
+   }
+
+   if(isPrime){
+        cout << num << " is a Prime Number" <<endl;
+    }
+    else{
+        cout << num << " is a Not Prime Number" <<endl;
+    }
+
+    return 0;
+}
