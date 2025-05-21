a3cdd33e465ce69c860a5057a10b9a88eab7e88b
Iam-samyog <msamyog37@gmail.com>
2025-05-21 22:10:03 +0545

    Modified the code of qn2.cpp

--- C++ codes/Day 2/qn2.cpp
+++ C++ codes/Day 2/qn2.cpp
@@ -8,8 +8,13 @@ int main(){
     cout << "Enter the Nth term:";
     cin >> n;
 
-    for(int i=1;i<=n;i++){
+    // for(int i=1;i<=n;i++){
+    //     sum+=i;
+    // }
+    int i=1;
+    while(i<=n){
         sum+=i;
+        i++;
     }
 
     cout << "Sum of Nth Term is " << sum;
