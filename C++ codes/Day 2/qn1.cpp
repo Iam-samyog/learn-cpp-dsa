d75a41ce2cc4b2afba290bddf55887c7e2ae8a88
Iam-samyog <msamyog37@gmail.com>
2025-05-21 21:32:59 +0545

    Modified the qn1.cpp

--- C++ codes/Day 2/qn1.cpp
+++ C++ codes/Day 2/qn1.cpp
@@ -10,10 +10,25 @@ int main(){
     cin >> ch;
 
     if(ch>=65 && ch<97){
-        cout << "The character is in Uppercase";
+        cout << "The character is in Uppercase\n";
     }
     else if(ch>=97 && ch<123 ){
-        cout << "The character is in Lowercase";
+        cout << "The character is in Lowercase\n";
+    }else{
+        cout << "Invalid Input\n";
     }
+
+    //another way is 
+    if(ch>='a' && ch<='z'){
+        cout << "The character is in Lowercase\n";
+    }
+    else if(ch>='A' && ch<='Z' ){
+        cout << "The character is in Uppercase\n";
+    }
+    else{
+        cout << "Invalid Input\n";
+    }
+
+
     return 0;
 }
