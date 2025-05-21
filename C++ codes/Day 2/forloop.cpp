163b582bd7fbdb6c505458ee7e45e668d76319bc
Iam-samyog <msamyog37@gmail.com>
2025-05-21 22:00:36 +0545

    Added forloop.cpp

---
+++ C++ codes/Day 2/forloop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //syntax
+    /*
+    for(initization;condition;updation){
+        //work
+    }
+    */
+    
+
+    for(int i=0;i<=20;i++){
+        cout << i << endl;
+    }
+    return 0;
+}
