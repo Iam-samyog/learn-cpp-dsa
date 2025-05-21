d896c5643eabc7296e74d359a272a6b5d964f7b1
Iam-samyog <msamyog37@gmail.com>
2025-05-21 23:00:42 +0545

    Modified the NestedLoop.cpp

--- C++ codes/Day 2/NestedLoop.cpp
+++ C++ codes/Day 2/NestedLoop.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main(){
     char ch='*';
-    for(int i=0;i<5;i++){
-        for(int j=0;j<=i;j++){
+    int n;
+    cout << "Enter the Nth term:";
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cout << i << " ";
+        for(int j=0;j<i;j++){
+            
             cout << ch << " ";
         }
         cout << endl;
