ef5af74202e087ce0f4df140d1c81ea388d7d082
Iam-samyog <msamyog37@gmail.com>
2025-05-22 22:09:21 +0545

    Added FloydAlpha.cpp

---
+++ C++ codes/Day-3/FloydAlph.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Nth term:";
+    cin >> n;
+    char alpha='A';
+    for(int i=0;i<n;i++){
+        for(int j=0; j<(i+1);j++){
+            
+            cout << alpha << " ";
+            alpha++;
+            
+        }
+        cout << endl;
+        
+    }
+    cout << endl;
+    return 0;
+}
