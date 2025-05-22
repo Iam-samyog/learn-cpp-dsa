5e4643ebcadc42ae7717951c4ecb1d74505b79b0
Iam-samyog <msamyog37@gmail.com>
2025-05-22 21:07:51 +0545

    Modified AlphabetPattern.cpp

--- C++ codes/Day-3/AlphabetPattern.cpp
+++ C++ codes/Day-3/AlphabetPattern.cpp
@@ -5,14 +5,15 @@ int main(){
     int n;
     cout << "Enter the Nth term:";
     cin >> n;
-    char alpha='A';
+    
     for(int i=0;i<n;i++){
+        char alpha='A';
         for(int j=0; j<n;j++){
             
             cout << alpha << " ";
             alpha++;
         }
-        alpha='A';
+        
         cout << endl;
     }
     cout << endl;
