445a82b2a85c408ebf5ee3619ffe6c11f986896d
Iam-samyog <msamyog37@gmail.com>
2025-05-22 22:29:37 +0545

    Added Pattern11.c

---
+++ C++ codes/Day-3/Pattern11.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Nth term:";
+    cin >> n;
+    
+    for(int i=0;i<n;i++){
+       
+    
+        for(int j=i; j>=0;j--){
+            char ch='A'+j;
+           
+            cout << ch << " ";
+           
+            
+            
+            
+        }
+        cout << endl;
+        
+    }
+    cout << endl;
+    return 0;
+}
