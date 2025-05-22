5128facfe0d014e313889acec9a8649a0c321d88
Iam-samyog <msamyog37@gmail.com>
2025-05-22 21:33:08 +0545

    Added NumberTri.cpp

---
+++ C++ codes/Day-3/NumberTri.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the nth term:";
+    cin >> n;
+    int k=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<(i+1);j++){
+            cout << k << " ";
+        }
+        k++;
+        cout << endl;
+    }
+    return 0;
+}
