3c7499256943f72e49868e4ec22e13aec3e08f56
Iam-samyog <msamyog37@gmail.com>
2025-05-23 23:27:39 +0545

    Modified the Pyramid Pattern .cpp

--- C++ codes/Day-3/PyramidPatter.cpp
+++ C++ codes/Day-3/PyramidPatter.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main(){
-    int n=4;
+    int n;
+    cout << "Enter the Nth term:"
+    cin  >> n;
     for(int i=0;i<n;i++){
         //space
         for(int j=0;j<(n-i-1);j++){
@@ -11,7 +13,9 @@ int main(){
         for(int j=0;j<(i+1);j++){
             cout << (j+1);
         }
-  
+        for(int j=i;j>=1;j--){
+            cout << j;
+        }
         cout << endl;
     }
      
