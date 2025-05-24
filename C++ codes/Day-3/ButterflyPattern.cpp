97cc931b9754066f52cb6829a7181bbef2ab2c70
Iam-samyog <msamyog37@gmail.com>
2025-05-24 22:22:27 +0545

    Added Butterfly pattern

---
+++ C++ codes/Day-3/ButterflyPattern.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    // Upper half
+    for(int i = 0; i < n; i++) {
+        // Left stars
+        for(int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        // Spaces
+        for(int j = 0; j < 2 * (n - i - 1); j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for(int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower half
+    for(int i = 0; i < n; i++) {
+        // Left stars
+        for(int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        // Spaces
+        for(int j = 0; j < 2 * i; j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for(int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
